1b696108a500560b6c94e8d1489037edb3b17ca6
Nicolas <nvh.develop@gmail.com>
2025-08-31 12:46:13 -0400

    fix definitivo cucumber

--- proyecto-java/src/test/java/com/develop/nvh/CucumberRunnerTest.java
+++ proyecto-java/src/test/java/com/develop/nvh/CucumberRunnerTest.java
@@ -6,10 +6,14 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features") // Ruta a la carpeta donde están los archivos .feature
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.develop.nvh.steps")
-public class CucumberRunnerTest {
-}
+@ConfigurationParameter(
+  key = PLUGIN_PROPERTY_NAME,
+  value = "pretty, summary, html:target/cucumber-report.html, json:target/cucumber.json"
+)
+public class CucumberRunnerTest { }
